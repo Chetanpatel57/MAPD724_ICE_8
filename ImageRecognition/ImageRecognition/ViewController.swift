6198579f95e80d61882e283fb4192aed9a03c8bb
Chetanpatel57 <40812586+Chetanpatel57@users.noreply.github.com>
2023-03-24 21:50:11 -0400

    Initial commit

---
+++ ImageRecognition/ImageRecognition/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  ImageRecognition
+//
+//  Created by Chetan Patel on 2023-03-24.
+//
+
+import UIKit
+import CoreML
+import Vision
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
