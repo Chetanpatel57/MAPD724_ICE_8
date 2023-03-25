1584c6ba1d6ee98eed47015f08b4c69ec24dbbcf
Chetanpatel57 <40812586+Chetanpatel57@users.noreply.github.com>
2023-03-24 22:03:45 -0400

    UI objects added

--- ImageRecognition/ImageRecognition/ViewController.swift
+++ ImageRecognition/ImageRecognition/ViewController.swift
@@ -10,10 +10,23 @@ import CoreML
 import Vision
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var labelDescription: UILabel!
+    
     override func viewDidLoad() {
+       
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let imagePath = Bundle.main.path(forResource: "car", ofType: "jpg")
+        let imageURL = NSURL.fileURL(withPath: imagePath!)
+        
+        let modelFile = MobileNetV2()
+        let model = try! VNCoreMLModel(for: modelFile.model)
+        
+        let handler = VNImageRequestHandler(url: imageURL)
+        let request = VNCoreMLRequest(model: model, completionHandler: findResults)
+        try! handler.perform([request])
+        
     }
 
 
